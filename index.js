d2124edcc6ac24e58c3383da792d0e44a07d5a01
Tyler Hall <thall2980@gmail.com>
2022-08-07 03:50:42 -0400

    commit tree 432ae7c68e726b7bd26a7be520afa95c3b5b03dc

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const array = ["Guadalupe", "Ollie", "Aki"];
+const eventName = "surprise"
+
+function writeCards(array, eventName) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        let message = `Thank you, ${array[i]}, for the wonderful ${eventName} gift!`;
+        newArray.push(message);
+    }
+
+    return newArray;
+}
+
+console.log(writeCards(array, eventName));
+
+
+function countDown(number) {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
